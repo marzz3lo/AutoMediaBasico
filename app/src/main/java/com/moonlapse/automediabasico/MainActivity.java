3241d88bc9ec8b9358c812eab36a3bc72fa907f8
marzz3lo <marzzelo@gmail.com>
2017-06-25 22:20:14 -0400

    Ejercicio: Lista de reproducción externa.

--- app/src/main/java/com/moonlapse/automediabasico/MainActivity.java
+++ app/src/main/java/com/moonlapse/automediabasico/MainActivity.java
@@ -1,13 +1,60 @@
 package com.moonlapse.automediabasico;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.this.getClass().getSimpleName();
+    private final String URL = "http://storage.googleapis.com/automotive-media/music.json";
+    private RequestQueue requestQueue;
+    private TextView txtLog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+        txtLog = (TextView) findViewById(R.id.txtLog);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //descargamos el fichero JSON mediante Volley
+                getRepositorioMusical();
+            }
+        });
+
     }
+
+    private void getRepositorioMusical() {
+        StringRequest request = new StringRequest(Request.Method.GET, URL, onPostsLoaded, onPostsError);
+        requestQueue.add(request);
+    }
+
+    private final Response.Listener<String> onPostsLoaded = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Log.i(TAG, response);
+            txtLog.setText(response);
+        }
+    };
+    private final Response.ErrorListener onPostsError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e(TAG, error.toString());
+            txtLog.setText(error.toString());
+        }
+    };
 }
